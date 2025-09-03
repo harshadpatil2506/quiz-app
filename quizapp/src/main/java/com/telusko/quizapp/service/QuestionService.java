30fdfb080d1b4862b61177aefcfa466ce5f57e92
Harshad Patil <harshadpatil25061998@gmail.com>
2025-09-03 18:14:51 +0530

    implemented getAllQuestions

---
+++ quizapp/src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.telusko.quizapp.service;
+
+import com.telusko.quizapp.dao.QuestionDao;
+import com.telusko.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return  questionDao.findAll();
+    }
+}
